aa204d0f5b7b60a04d0b93627ce6a4e4e7078f1f
Rohit Raj <rajrohit5783@gmail.com>
2026-08-30 09:52:59 +0530

    Modules

--- 01_Hello_world.js
+++ 01_Hello_world.js
@@ -1,2 +1,9 @@
-console.log("Hey there ! I am JS");
+const math = require('./math');
+const BModules = reqire("fs");
 
+
+// console.log("Hey there ! I am JS");
+
+
+
+console.log("Math value is ", math.add(2,4));
