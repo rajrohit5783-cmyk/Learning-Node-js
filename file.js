29bd9fc8a96e89efbcebefd662c5a1b71ca6a2aa
Rohit Raj <rajrohit5783@gmail.com>
2026-08-30 14:12:10 +0530

    Node basics

--- file.js
+++ file.js
@@ -1,15 +1,20 @@
-const { captureRejectionSymbol } = require("events");
+// const { captureRejectionSymbol } = require("events");
 const fs = require("fs");
+const os = require("os");
 
 
-// //Sync... 
-// // fs.writeFileSync('./text.txt', 'Hey There');
+console.log(os.cpus().lenth);
 
 
-// //Async
 
-// fs.writeFile("./text.txt", "Hello World Async", (err) => { }
-// );
+//Sync...  
+// fs.writeFileSync('./text.txt', 'Hey There');
+
+
+//Async
+
+fs.writeFile("./text.txt", "Hello World Async", (err) => { }
+);
 
 
 // const result = fs.readFileSync('./contacts.txt', "utf-8")
@@ -22,15 +27,40 @@ const fs = require("fs");
 //     else{
 //         console.log(result);
 //     }
-// });
-fs.appendFileSync("./text.txt",`${Date.now()} hey there\n`);
+// // });
+// fs.appendFileSync("./text.txt",`${Date.now()} hey there\n`);
 
-// fs.copyFileSync('./text.txt','./copy.txt' );
+// // fs.copyFileSync('./text.txt','./copy.txt' );
 
-// fs.unlinkSync('./copy.txt');
+// // fs.unlinkSync('./copy.txt');
 
-console.log(fs.statSync('./text.txt'));
+// console.log(fs.statSync('./text.txt'));
 
-fs.mkdirSync("my-docss/a/b", {recursive: true});
+// fs.mkdirSync("my-docss/a/b", {recursive: true});
 
+
+console.log('1');
+
+//Blocking..
+
+// const result = fs.readFileSync('./contacts.txt', 'utf-8');
+// console.log(result);
+
+// console.log('2');
+
+
+//non-Blocking..
+
+fs.readFile('./contacts.txt','utf-8', (err, result) => {
+    console.log(result);
+
+
+})
+
+console.log('2');
+console.log('3');
+console.log('4');
+
+
+//Default Thread pool size = 4
 
