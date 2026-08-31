d594a16dfc7161513cdc745ac288d45b31889b84
Rohit Raj <rajrohit5783@gmail.com>
2026-08-31 16:24:29 +0530

    http methods and Express

--- server/index.js
+++ server/index.js
@@ -1,46 +1,77 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
+// const http = require("http");
+// const fs = require("fs");
+// const url = require("url");
+const express = require("express");
 
-const myServer = http.createServer((req, res) => {
 
-    if (req.url === "/favicon.ico") return res.end();
+const app = express();
 
-    const log = `${Date.now()}: ${req.url} New Req Received\n`;
+app.get('/',(req,res) => {
+    return res.send("Hello From Home Page");
+})
 
-    const myUrl = url.parse(req.url, true);
+app.get('/about',(req,res) => {
+    return res.send(`Hello ${req.query.name}`)
+})
 
-    console.log(myUrl);
 
-    fs.appendFile("log.txt", log, (err) => {
 
-        if (err) {
-            console.log(err);
-            return res.end("Something went wrong");
-        }
+// function myHandler(req, res) {
+//     if (req.url === "/favicon.ico") return res.end();
 
-        switch (myUrl.pathname) {
+//     const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
 
-            case "/":
-                res.end("HomePage");
-                break;
+//     const myUrl = url.parse(req.url, true);
 
-            case "/about":
-                const username = myUrl.query.myName;
-                res.end(`Hi ${username}`);
+//     console.log(myUrl);
 
-                break;
+//     fs.appendFile("log.txt", log, (err) => {
 
-            case "/search":
-                const search = myUrl.query.search_query;
-                res.end("Here are your results for " + search);
+//         if (err) {
+//             console.log(err);
+//             return res.end("Something went wrong");
+//         }
 
-            default:
-                res.end("404 Not Found");
-        }
-    });
-});
+//         switch (myUrl.pathname) {
 
-myServer.listen(8000, () => {
-    console.log("Server Started");
-});
+//             case "/":
+//                 if (req.method === "GET") res.end("HomePage");
+//                 break;
+
+//             case "/about":
+//                 const username = myUrl.query.myName;
+//                 res.end(`Hi ${username}`);
+
+//                 break;
+
+//             case "/search":
+//                 const search = myUrl.query.search_query;
+//                 res.end("Here are your results for " + search);
+
+//             case "/signup":
+//                 if (req.method === "GET") res.end("This is a signup form");
+//                 else if (req.method === "POST") {
+//                     //DB Query
+//                     res.end("Success");
+//                 }
+//                 break;
+//             default:
+//                 res.end("404 Not Found");
+//         }
+//     });
+// };
+
+
+
+app.listen(8000,() => console.log("Server Started"))
+
+
+
+//This can be also used in express
+
+
+// const myServer = http.createServer(app);
+
+// myServer.listen(8000, () => {
+//     console.log("Server Started");
+// });
