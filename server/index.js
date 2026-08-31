9ac3dabdb89fd4bfe56255e919c90a27a5401954
Rohit Raj <rajrohit5783@gmail.com>
2026-08-31 08:23:01 +0530

    http server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const http = require ("http");
+const fs = require('fs');
+
+
+const myServer = http.createServer((req, res) => {
+
+    const log = `${Date.now()}: ${req.url} New Req Received\n `;
+    fs.appendFile('log.txt', log,(err,data)  => {
+        switch(req.url){
+            case "/":
+                res.end("HomePage");
+                break; 
+            case "/about":
+                res.end("I am Rohit Raj")
+                break;
+            default: 
+            res.end("404 Not Found") ;
+
+        }
+    })
+    
+});
+
+myServer.listen(8000,() => console.log("Server Started"));
+
+
+
