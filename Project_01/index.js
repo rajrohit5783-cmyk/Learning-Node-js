1176eed54dcb0c9ab3090143cba089bb8b93a6e6
Rohit Raj <rajrohit5783@gmail.com>
2026-09-02 22:53:24 +0530

    Format and delete in REST API

--- Project_01/index.js
+++ Project_01/index.js
@@ -13,39 +13,34 @@ app.get("/users", (req, res) => {
 
     //REST API
 
-app.get("/api/users", (req, res) => {
-    return res.json(users);
-});
+    app.get("/api/users", (req, res) => {
+        return res.json(users);
+    });
 
 
     res.send(html);
 });
 
 
-app.route('/api/users/:id').get((req,res) => {
-    const id  =  Number(req.params.id);
+app.route('/api/users/:id').get((req, res) => {
+    const id = Number(req.params.id);
     const user = users.find(user => user.id === id);
     return res.json(user);
 
-}).patch(req,res => {
-     //Edit with user ID
-     res.join.json({status: "Pending"});
+}).patch(req, res => {
+    //Edit with user ID
+    res.join.json({ status: "Pending" });
 })
-.delete((req,res) => {
-    //Delete user with id
-    res.join.json({status: "Pending"});
-});
+    .delete((req, res) => {
+        //Delete user with id
+        res.join.json({ status: "Pending" });
+    });
 
-app.post('/api/users', (req,res)=> {
+app.post('/api/users', (req, res) => {
     //TODO: Create a new user
-    return res.join({status: "pending"});
+    return res.join({ status: "pending" });
 
 });
-
-
-
-
-
 
 app.listen(PORT, () => {
     console.log(`Server Started at PORT ${PORT}`);
